05296cd949f9b04e203ce3bdad0507804bd1b003
chielbleyenbergh <chielbleynbergh@gmail.com>
2019-02-01 09:43:00 +0100

    Finished Design, Added Controller for Podiums -> finished table Added Model: Artist, Artists, Podium, Podiums

---
+++ IOS project Chiel/Model/ArtistsDAO.swift
@@ -0,0 +1,21 @@
+//
+//  ArtistsDAO.swift
+//  IOS project Chiel
+//
+//  Created by chiel bleyenbergh on 01/02/2019.
+//  Copyright © 2019 ehb. All rights reserved.
+//
+
+import Foundation
+
+class ArtistsDAO {
+    
+    var Artists:[Artist]
+    
+    init() {
+        let ReelBigFish = Artist.init(name: "ReelBigFish", genre: "R&B)
+        
+        
+    }
+    
+}
