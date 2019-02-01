d58933bf77c87548b46010d05670e818459d5249
chielbleyenbergh <chielbleynbergh@gmail.com>
2019-02-01 10:29:39 +0100

    Finished Model: Artists Added controller for Astrists, finished table

---
+++ IOS project Chiel/Controller/ArtistViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ArtistViewController.swift
+//  IOS project Chiel
+//
+//  Created by chiel bleyenbergh on 01/02/2019.
+//  Copyright © 2019 ehb. All rights reserved.
+//
+
+import UIKit
+
+class ArtistViewController: UIViewController, UITabBarDelegate, UITableViewDataSource {
+    
+    
+    @IBOutlet weak var tblView: UITableView!
+    let artists = ArtistsDAO.init()
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return artists.Artists.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cellArtists")!
+        
+        let artist = artists.Artists[indexPath.row]
+        
+        cell.textLabel!.text = artist.name
+        
+        return cell
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- IOS project Chiel/Model/ArtistsDAO.swift
+++ IOS project Chiel/Model/ArtistsDAO.swift
@@ -13,8 +13,14 @@ class ArtistsDAO {
     var Artists:[Artist]
     
     init() {
-        let ReelBigFish = Artist.init(name: "ReelBigFish", genre: "R&B)
+        let reelBigFish = Artist.init(name: "Reel Big Fish", genre: "R&B")
+        let swashbuckle = Artist.init(name: "Swashbuckle", genre: "Hardcore")
+        let admiralFreebie = Artist.init(name: "Admiral Freebie", genre: "Techno")
+        let siameseFighting = Artist.init(name: "Siamese Fighting", genre: "House")
+        let goldfish = Artist.init(name: "Goldfish", genre: "Deep House")
+        let zander = Artist.init(name: "Zander", genre: "drum and bass")
         
+        self.Artists = [reelBigFish, swashbuckle, admiralFreebie, siameseFighting, goldfish, zander]
         
     }
     
